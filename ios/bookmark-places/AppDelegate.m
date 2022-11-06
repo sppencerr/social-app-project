6fc56b862617077acd645c0314d91574f68e2b11
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-11-05 23:57:15 -0600

    delegate

---
+++ ios/bookmark-places/AppDelegate.m
@@ -0,0 +1,26 @@
+
+#import "AppDelegate.h"
+#import <React/RCTBundleURLProvider.h>
+
+#import "RCCManager.h"
+
+#import <React/RCTRootView.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  NSURL *jsCodeLocation;
+#ifdef DEBUG
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+  
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  self.window.backgroundColor = [UIColor whiteColor];
+  [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
+  return YES;
+}
+
+@end
